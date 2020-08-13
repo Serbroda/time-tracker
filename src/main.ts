d9b13dd4f7c6a47d8832377845d578202b6bd548
Danny Rottstegge <danny@rottstegge.net>
2020-08-13 23:34:10 +0200

    Fix script imports in html

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as path from 'path';
+import { ipcMain, BrowserWindow } from 'electron';
+
 // @ts-ignore
 const { menubar } = require('menubar');
 
@@ -6,8 +8,25 @@ const mb = menubar({
     cwd: __dirname,
     index: `file://${path.join(__dirname, 'index.html')}`,
     icon: path.join(path.join(__dirname, 'time.png')),
+    browserWindow: {
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    },
 });
 
 mb.on('ready', () => {
-    console.log('Menubar app is ready.');
+    console.log('Menubar app is ready.', mb);
+});
+
+mb.on('show', () => {
+    console.log('Show');
+    const win = mb.window as BrowserWindow;
+    if (win && win.webContents) {
+        win.webContents.openDevTools();
+    }
+});
+
+ipcMain.on('start', (event: any) => {
+    console.log('Main: start');
 });
